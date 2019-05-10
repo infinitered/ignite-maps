ce605de5afd593b6e627342220d934337c0e85c1
Ryan Linton <ryan@infinite.red>
2019-05-10 14:45:48 -0700

    Update commands to support ignite 3

--- commands/map-callout.js
+++ commands/map-callout.js
@@ -1,30 +1,29 @@
-// @cliDescription  Generates a map callout component.
-// @cliExample  ignite generate map-callout StoreCallout
-// Generates a map callout component that can be inserted into any map.
-// ----------------------------------------------------------------------------
-module.exports = async function (context) {
-  const { parameters, strings, print, ignite } = context
-  const { pascalCase, isBlank } = strings
+module.exports = {
+  description: 'Generates a map callout component.',
+  run: async function (context) {
+    const { parameters, strings, print, ignite } = context
+    const { pascalCase, isBlank } = strings
 
-  // validation
-  if (isBlank(parameters.first)) {
-    print.info(`ignite generate map-callout <name>\n`)
-    print.info('A name is required.')
-    return
+    // validation
+    if (isBlank(parameters.first)) {
+      print.info(`ignite generate map-callout <name>\n`)
+      print.info('A name is required.')
+      return
+    }
+
+    const name = pascalCase(parameters.first)
+
+    // Copies the callout template
+    // into App/Components/${name}.js.
+    const copyJobs = [{
+      template: 'map-callout-component.js.ejs',
+      target: `App/Components/${name}.js`
+    }, {
+      template: 'map-callout-styles.js.ejs',
+      target: `App/Components/Styles/${name}Styles.js`
+    }]
+
+    // make the templates and pass in props with the third argument here
+    await ignite.copyBatch(context, copyJobs, { calloutName: name })
   }
-
-  const name = pascalCase(parameters.first)
-
-  // Copies the callout template
-  // into App/Components/${name}.js.
-  const copyJobs = [{
-    template: 'map-callout-component.js.ejs',
-    target: `App/Components/${name}.js`
-  }, {
-    template: 'map-callout-styles.js.ejs',
-    target: `App/Components/Styles/${name}Styles.js`
-  }]
-
-  // make the templates and pass in props with the third argument here
-  await ignite.copyBatch(context, copyJobs, { calloutName: name })
 }
